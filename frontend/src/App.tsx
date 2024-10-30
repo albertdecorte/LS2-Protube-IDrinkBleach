621b384cea1d1aaaf15d967a08ae9fb0380b58cb
adecorte <adecorte@edu.tecnocampus.cat>
2024-10-30 11:14:30 +0100

    subiendo el patch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
+import logo from './assets/logo.svg';
 import './App.css';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { useAuth0 } from '@auth0/auth0-react';
-import Finallogo from './assets/Finallogo.svg';
 
+
+// This is your entry point
+// Feel free to modify ANYTHING in this file
 // Substitueix aquests valors amb els teus
 const domain = "dev-r7hj507hsi3jn34i.us.auth0.com";
 const clientId = "g84SYUoiDvFIGevVYEBH5AcB4xaoHUFZ";
@@ -10,7 +11,6 @@ const clientId = "g84SYUoiDvFIGevVYEBH5AcB4xaoHUFZ";
 // Component per mostrar botons de Login/Logout
 const AuthButtons = () => {
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
-
   return (
       <div>
         {!isAuthenticated && (
@@ -30,34 +30,24 @@ const AuthButtons = () => {
 };
 
 function App() {
-    return (
-      <Auth0Provider
-          domain={domain}
-          clientId={clientId}
-          authorizationParams={{
-            redirect_uri: window.location.origin
-          }}
-      >
-        <div className="App">
-          <header className="App-header">
-            <img src={Finallogo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            <AuthButtons /> {/* Afegim el component de login/logout */}
-          </header>
-        </div>
-      </Auth0Provider>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
 export default App;
-
