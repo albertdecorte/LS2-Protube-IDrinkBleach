8d076d9a9ac4a55ab306ec788cbba2cf25e07f7f
adecorte <adecorte@edu.tecnocampus.cat>
2024-10-30 11:14:30 +0100

    subiendo el patch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,33 @@
 import logo from './assets/logo.svg';
 import './App.css';
 
+
 // This is your entry point
 // Feel free to modify ANYTHING in this file
+// Substitueix aquests valors amb els teus
+const domain = "dev-r7hj507hsi3jn34i.us.auth0.com";
+const clientId = "g84SYUoiDvFIGevVYEBH5AcB4xaoHUFZ";
+
+// Component per mostrar botons de Login/Logout
+const AuthButtons = () => {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+  return (
+      <div>
+        {!isAuthenticated && (
+            <button onClick={() => loginWithRedirect()}>Login</button>
+        )}
+
+        {isAuthenticated && (
+            <>
+              <button onClick={() => logout({ returnTo: window.location.origin } as any)}>
+                Logout
+              </button>
+              <p>Has iniciat sessió correctament!</p>
+            </>
+        )}
+      </div>
+  );
+};
 
 function App() {
   return (
