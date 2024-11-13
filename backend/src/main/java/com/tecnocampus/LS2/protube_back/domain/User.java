787e62add184dd3c6fa1286a6fd70ff1f96536be
hugRibaSendra <hriba@edu.tecnocampus.cat>
2024-11-13 16:10:51 +0100

    Video Entity

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/domain/User.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/domain/User.java
@@ -19,4 +19,28 @@ public class User {
     }
 
     public User() {}
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getGmail() {
+        return gmail;
+    }
+
+    public void setGmail(String gmail) {
+        this.gmail = gmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
